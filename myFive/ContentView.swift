c880da1ec650a83ab0c027bd9238abe0ae53826a
andre-tirta <andretirtawijaya@gmail.com>
2024-12-19 19:41:53 +0700

    Initial Commit

---
+++ myFive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myFive
+//
+//  Created by Andre Tirta Wijaya on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myFive/myFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFiveApp.swift
+//  myFive
+//
+//  Created by Andre Tirta Wijaya on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct myFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
